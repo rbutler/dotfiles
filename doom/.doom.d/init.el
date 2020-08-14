27f95691899459afa3b80c037dfc8710a7c4b756
Ryan Butler <1170939+rbutler@users.noreply.github.com>
2020-08-14 08:00:16 -0500

    protos

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -113,7 +113,8 @@
        ;;faust             ; dsp, but you get to keep your soul
        ;;fsharp           ; ML stands for Microsoft's Language
        (go +lsp)               ; the hipster dialect
-       (haskell +intero) ; a language that's lazier than I am
+       (haskell +lsp) ; a language that's lazier than I am
+       ;(haskell +intero) ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
@@ -138,6 +139,7 @@
        ;;perl              ; write code no one else can comprehend
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
+       protobuf
        ;;purescript        ; javascript, but functional
        ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
